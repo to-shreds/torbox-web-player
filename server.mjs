87ea4c823b445f939be7b273584e9c5941b8e1d6
to-shreds <jonathanjablon@gmail.com>
2026-09-18 22:09:12 -0400

    Align v0.7 fallback modules and runtime version

--- server.mjs
+++ server.mjs
@@ -14,6 +14,7 @@ const publicFiles = new Map([
   ['/setup.html', ['setup.html', 'text/html; charset=utf-8']],
   ['/app.js', ['app.js', 'text/javascript; charset=utf-8']],
   ['/runtime.js', ['runtime.js', 'text/javascript; charset=utf-8']],
+  ['/history.js', ['history.js', 'text/javascript; charset=utf-8']],
   ['/playback-errors.js', ['playback-errors.js', 'text/javascript; charset=utf-8']],
   ['/discover.js', ['discover.js', 'text/javascript; charset=utf-8']],
   ['/source-client.js', ['source-client.js', 'text/javascript; charset=utf-8']],
