c4b1ac7783572ea61645a5a3890f905dd4e8f55d
to-shreds <jonathanjablon@gmail.com>
2026-09-20 00:29:31 -0400

    Audit release integrity: public/direct-runtime.js

--- public/direct-runtime.js
+++ public/direct-runtime.js
@@ -1,7 +1,7 @@
-import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.0.7';
-import { isTrustedDirectMediaUrl } from './runtime.js?v=2.0.7';
+import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.0.8';
+import { isTrustedDirectMediaUrl } from './runtime.js?v=2.0.8';
 
-export const DIRECT_BUILD = 'browser-local-2.0.7';
+export const DIRECT_BUILD = 'browser-local-2.0.8';
 
 const CATALOG_PRIMARY = 'https://v3-cinemeta.strem.io';
 const CATALOG_SECONDARY = 'https://cinemeta-catalogs.strem.io';
