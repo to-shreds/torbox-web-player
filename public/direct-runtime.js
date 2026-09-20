f4b587760e1e7eb1bdb00909394e130a62ccaade
to-shreds <jonathanjablon@gmail.com>
2026-09-20 01:27:55 -0400

    Align public/direct-runtime.js with 2.2.0

--- public/direct-runtime.js
+++ public/direct-runtime.js
@@ -1,7 +1,7 @@
-import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.1.0';
-import { isTrustedDirectMediaUrl } from './runtime.js?v=2.1.0';
+import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.2.0';
+import { isTrustedDirectMediaUrl } from './runtime.js?v=2.2.0';
 
-export const DIRECT_BUILD = 'browser-local-2.1.0';
+export const DIRECT_BUILD = 'browser-local-2.2.0';
 
 const CATALOG_PRIMARY = 'https://v3-cinemeta.strem.io';
 const CATALOG_SECONDARY = 'https://cinemeta-catalogs.strem.io';
