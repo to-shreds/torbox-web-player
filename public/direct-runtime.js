e68fa39bf4ae440ad0a94dd2b67ad564422ba68a
to-shreds <jonathanjablon@gmail.com>
2026-09-20 10:00:27 -0400

    Bump public/direct-runtime.js for search repair 2.2.1

--- public/direct-runtime.js
+++ public/direct-runtime.js
@@ -1,7 +1,7 @@
-import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.2.0';
-import { isTrustedDirectMediaUrl } from './runtime.js?v=2.2.0';
+import { normalizeSources, targetOf, cleanText, parseSizeBytes } from './source-client.js?v=2.2.1';
+import { isTrustedDirectMediaUrl } from './runtime.js?v=2.2.1';
 
-export const DIRECT_BUILD = 'browser-local-2.2.0';
+export const DIRECT_BUILD = 'browser-local-2.2.1';
 
 const CATALOG_PRIMARY = 'https://v3-cinemeta.strem.io';
 const CATALOG_SECONDARY = 'https://cinemeta-catalogs.strem.io';
