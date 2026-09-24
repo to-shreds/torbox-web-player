5cffc87663c90eafd2fe3a0d97b17e51d2447207
to-shreds <jonathanjablon@gmail.com>
2026-09-24 01:00:02 -0400

    Bump public/playback-errors.js cache build to restored12

--- public/playback-errors.js
+++ public/playback-errors.js
@@ -1,4 +1,4 @@
-import { isTrustedMediaUrl, isTrustedRelayMediaUrl } from './runtime.js?v=restored11';
+import { isTrustedMediaUrl, isTrustedRelayMediaUrl } from './runtime.js?v=restored12';
 export function matchesFormat(file, format = 'all') {
   return format !== 'mp4' || /\.mp4$/i.test(file.title || '');
 }
